fecbfc36e675f0354576dfa3fd1a244ca18aa8e4
arashhaji <56742302+arashhaji@users.noreply.github.com>
2020-02-29 22:49:20 -0500

    update

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const knex = require("knex");
 const config = require("../knexfile");
-const env = process.env.DB_ENV || "development";
+const env = process.env.DB_ENV
 module.exports = knex(config[env]);
